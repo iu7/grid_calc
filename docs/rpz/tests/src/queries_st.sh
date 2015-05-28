8b9f0ea82c001269aed425f6e22b6783b1051768
Коробков Сергей <sergs933@gmail.com>
2015-05-28 18:59:24 +0300

    formatted fs system test

--- docs/rpz/tests/src/queries_st.sh
+++ docs/rpz/tests/src/queries_st.sh
@@ -6,21 +6,24 @@ g++ test.cpp -o a.out
 
 set -v
 
-curl @"localhost:50002/static?path=1\2\3\4"  -X POST -F file=@a.out; echo
 curl @"localhost:50002/static"               -X POST -F file=@a.out; echo
-
-curl @"localhost:50002/static/1\2\3\4\a.out" -X GET > a_.out       ; echo
-chmod +x a_.out                                                    ; echo
-./a_.out                                                           ; echo
+curl @"localhost:50002/static?path=1\2\3\4"  -X POST -F file=@a.out; echo
 
 curl @"localhost:50002/static/a.out"         -X GET > a__.out      ; echo
 chmod +x a__.out                                                   ; echo
 ./a__.out                                                          ; echo
 
-curl @"localhost:50002/static/1\2\3\4\a.out" -X DELETE             ; echo
-curl @"localhost:50002/static/a.out"         -X DELETE             ; echo
+curl @"localhost:50002/static/1\2\3\4\a.out" -X GET > a_.out       ; echo
+chmod +x a_.out                                                    ; echo
+./a_.out                                                           ; echo
 
-curl @"localhost:50002/static/1\2\3\4\a.out" -X GET                ; echo
+curl @"localhost:50002/static/a.out"         -X DELETE             ; echo
+curl @"localhost:50002/static/1\2\3\4\a.out" -X DELETE             ; echo
+
+curl @"localhost:50002/static/a.out"         -X DELETE             ; echo
+curl @"localhost:50002/static/1\2\3\4\a.out" -X DELETE             ; echo
+
 curl @"localhost:50002/static/a.out"         -X GET                ; echo
+curl @"localhost:50002/static/1\2\3\4\a.out" -X GET                ; echo
 
 rm a*.out test.cpp
